4cb8d333ab3e154b22613b325d580c6e1d7721cf
Krishan Patel <krishan711@gmail.com>
2023-09-01 09:43:08 +0100

    Fiddle: add infra with terraform + move deployment to new box (#3)
    * .
    
    * .

---
+++ infra/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_version = "~> 1.3.7"
+
+  # aws s3 --profile kiba mb s3://kiba-infra-assets-proxy
+  backend "s3" {
+    key = "tf-state.json"
+    region = "eu-west-1"
+    bucket = "kiba-infra-assets-proxy"
+    profile = "kiba"
+    encrypt = true
+  }
+
+  required_providers {
+    aws = {
+      version = "5.14.0"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "kiba"
+  region = "eu-west-1"
+}
+
+locals {
+  project = "assets-proxy"
+}
